07fb5e8ad367258aa33cd99d50298bc3d0619428
Lucas Jennings <lucas@Lucass-MacBook-Pro.local>
2022-06-19 21:13:37 -0400

    Finishing GET calls for tokens

--- routes/tokens.js
+++ routes/tokens.js
@@ -10,22 +10,55 @@ router.get('/', async(req, res) => {
     let metadata = req.query.metadata === 'true';
     let prices = req.query.prices === 'true';
     let tokenIds;
+    let tokenData;
     if (!getSaleTokens && !metadata) {
         try {
-            tokenIds = await queries.getAllTokenIds();
+            tokenData = await queries.getAllTokenIds();
         } catch (err) {
             console.log(`DB Query for getting all tokens failed: ${err}`);
             res.status(500).send('Uh oh, something went wrong...');
         }
-    } else if (!metadata) {
+    } else if (!getSaleTokens) {
+        try {
+            tokenData = await queries.getAllTokenMetadata();
+        } catch (err) {
+            console.log(`DB Query for getting all tokens & metadata failed: ${err}`);
+            res.status(500).send('Uh oh, something went wrong...');
+        }
+    }
+    else  {
         try {
             tokenIds = await methods.getSaleTokenIds();
+            tokenData = [];
+            for (i = 0; i < tokenIds.length; i++) {
+                tokenData.push(
+                    {
+                        "token_id": tokenIds[i]
+                    }
+                )
+            }
         } catch (err) {
             console.log(`Blockchain Query for getting sale tokens failed: ${err}`);
             res.status(500).send('Uh oh, something went wrong...');
         }
     }
-    res.status(200).send(tokenIds);
+    if (getSaleTokens && metadata) {
+        try {
+            tokenData = await queries.queryByTokenIds(tokenIds);
+        } catch (err) {
+            console.log(`DB Query for getting token: ${tokenIds} metadata failed: ${err}`);
+            res.status(500).send('Uh oh, something went wrong...');
+        }
+    }
+    if (prices) {
+        try {
+            tokenData = await methods.getTokenPrice(tokenData);
+        } catch (err) {
+            console.log(`Blockchain Query for getting token prices failed: ${err}`);
+            res.status(500).send('Uh oh, something went wrong...');
+        }
+    }
+    res.status(200).send(tokenData);
 
 })
 
