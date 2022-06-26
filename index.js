258ee756118b01523b64c8aed39bd290d9321175
Lucas Jennings <lucas@Lucass-MacBook-Pro.local>
2022-06-26 10:08:49 -0400

    Adding CORS

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ require('./startup/database.js')();
 const lushVault = require('./startup/blockchain.js');
 require('./startup/morgan.js')(app);
 require('./startup/routes.js')(app);
+const cors = require('cors');
+app.use(cors({
+  origin: '*'
+}));
 
 // Default to port 3000 if none specified in the environment
 const port = process.env.PORT || '3000';
